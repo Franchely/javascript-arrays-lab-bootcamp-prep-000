a0e40721b401c33275a60b62d012f51dbcf7e874
Franchely Portorreal <franchely811@gmail.com>
2019-05-01 14:36:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop("Garfield")
+  return kittens 
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift("Milo")
+  return kittens
+}
+
+function appendKitten(name) {
+ var appendKitten = kittens.splice(2, 0, "Broom")
+ return appendKitten 
+}
